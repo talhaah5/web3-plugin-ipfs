86a1f9af2d8371bcb01958546326668b1248b6ba
Talha Ahmed <talhaah5@gmail.com>
2023-11-11 22:29:58 +0100

    feat: Implementation of Contract Call To Store CID (#2)

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,18 +7,37 @@ describe("TemplatePlugin Tests", () => {
   const secret = "";
   it("should register IPFSPlugins on Web3Context instance", () => {
     const web3Context = new core.Web3Context("http://127.0.0.1:8545");
-    web3Context.registerPlugin(new IPFSPlugin(host, apiKey, secret));
+    web3Context.registerPlugin(
+      new IPFSPlugin({
+        ipfsHost: host,
+        ipfsApiKey: apiKey,
+        ipfsSecretKey: secret,
+      })
+    );
     expect(web3Context.ipfs).toBeDefined();
   });
 
   describe("IPFSPlugins method tests", () => {
     let web3: Web3;
 
-    xit("should store file on IPFS", async () => {
-      web3 = new Web3("http://127.0.0.1:8545");
-      web3.registerPlugin(new IPFSPlugin(host, apiKey, secret));
+    xit("should store file on IPFS and register to contract", async () => {
+      web3 = new Web3("https://sepolia.drpc.org");
+      web3.eth.accounts.wallet.add(
+        "PRIVATE_KEY"
+      );
+      web3.registerPlugin(
+        new IPFSPlugin({
+          ipfsHost: host,
+          ipfsApiKey: apiKey,
+          ipfsSecretKey: secret,
+        })
+      );
       const cid = await web3.ipfs.storeFile("test/test.txt");
+      await web3.ipfs.sendTransactionToRegistry(
+        web3.eth.accounts.wallet[0].address,
+        cid.toString()
+      );
       console.log(cid);
-    });
+    }, 70000);
   });
 });
