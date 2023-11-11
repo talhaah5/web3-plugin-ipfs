2f8411e05a2ca46ec6b94fe5ecd08f27550f4ddd
talha <talha@seed.im>
2023-11-11 22:26:19 +0100

    removed lint check and added comment of issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { readFileSync } from "fs";
 import { CID } from "ipfs-http-client";
-import { Web3PluginBase ,Address, Contract, ContractAbi, Web3Context, TransactionReceipt } from "web3";
+import {
+  Web3PluginBase,
+  Address,
+  Contract,
+  ContractAbi,
+  Web3Context,
+  TransactionReceipt,
+} from "web3";
 import { IpfsClient } from "./utils/ipfs-client";
 import { REGISTRY_ABI } from "./contracts/registry";
 import { REGISTRY_CONTRACT_ADDRESS } from "./constants";
@@ -9,24 +16,28 @@ export class IPFSPlugin extends Web3PluginBase {
   public pluginNamespace = "ipfs";
   private readonly registryContract: Contract<typeof REGISTRY_ABI>;
 
-  constructor(private options: {
-    ipfsHost: string,
-    ipfsApiKey: string,
-    ipfsSecretKey: string,
-    registryAbi?: ContractAbi,
-    registryContractAddress?: Address
-  }) {
+  constructor(
+    private options: {
+      ipfsHost: string;
+      ipfsApiKey: string;
+      ipfsSecretKey: string;
+      registryAbi?: ContractAbi;
+      registryContractAddress?: Address;
+    }
+  ) {
     super();
 
-    this.options.registryAbi =
-			options?.registryAbi ?? REGISTRY_ABI;
+    this.options.registryAbi = options?.registryAbi ?? REGISTRY_ABI;
 
     this.options.registryContractAddress =
-			options?.registryContractAddress ?? REGISTRY_CONTRACT_ADDRESS;
+      options?.registryContractAddress ?? REGISTRY_CONTRACT_ADDRESS;
 
-    this.registryContract = new Contract<typeof REGISTRY_ABI>(this.options.registryAbi, this.options.registryContractAddress);
- 
-    this.link(this)
+    this.registryContract = new Contract<typeof REGISTRY_ABI>(
+      this.options.registryAbi,
+      this.options.registryContractAddress
+    );
+
+    this.link(this);
   }
 
   async storeFile(fileSrc: string): Promise<CID> {
@@ -48,33 +59,32 @@ export class IPFSPlugin extends Web3PluginBase {
 
   async sendTransactionToRegistry(
     account: string,
-    cid: string,
+    cid: string
   ): Promise<TransactionReceipt> {
     try {
-
       if (this.registryContract.methods.store === undefined) {
-        throw new Error(
-          'Provided registryAbi is missing store method',
-        );
+        throw new Error("Provided registryAbi is missing store method");
       }
 
-      const tx: TransactionReceipt = await (await (this.registryContract.methods as any)
-      .store(cid)
-      .send({
-        from: account,
-      }));
+      const tx: TransactionReceipt = await await (
+        this.registryContract.methods as any //added any because of issue https://github.com/web3/web3.js/issues/6275
+      )
+        .store(cid)
+        .send({
+          from: account,
+        });
 
       return tx;
     } catch (error) {
-      console.error('An error occurred during the transaction process:', error)
-      throw error
+      console.error("An error occurred during the transaction process:", error);
+      throw error;
     }
   }
 
   public link(parentContext: Web3Context) {
     super.link(parentContext);
     this.registryContract.link(parentContext);
-}
+  }
 }
 
 // Module Augmentation
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,15 +3,17 @@ import { IPFSPlugin } from "../src";
 
 describe("TemplatePlugin Tests", () => {
   const host = "ipfs.infura.io:5001";
-  const apiKey = '2Y0JPCktAnUI9DTMERJ3BfIvs9Q'
-  const secret = '494bfcba49e00d9a9d5fcc5bd1e6c3b2'
+  const apiKey = "2Y0JPCktAnUI9DTMERJ3BfIvs9Q";
+  const secret = "494bfcba49e00d9a9d5fcc5bd1e6c3b2";
   it("should register IPFSPlugins on Web3Context instance", () => {
     const web3Context = new core.Web3Context("http://127.0.0.1:8545");
-    web3Context.registerPlugin(new IPFSPlugin({
-      ipfsHost: host, 
-      ipfsApiKey: apiKey, 
-      ipfsSecretKey: secret
-    }));
+    web3Context.registerPlugin(
+      new IPFSPlugin({
+        ipfsHost: host,
+        ipfsApiKey: apiKey,
+        ipfsSecretKey: secret,
+      })
+    );
     expect(web3Context.ipfs).toBeDefined();
   });
 
@@ -19,16 +21,23 @@ describe("TemplatePlugin Tests", () => {
     let web3: Web3;
 
     it("should store file on IPFS and register to contract", async () => {
-      web3 = new Web3(("https://sepolia.drpc.org"));
-      web3.eth.accounts.wallet.add('0xf0ce7ced295a088f3673d764adf52bdd48110ee5efe39ebf02ca3ac18453bad0');
-      web3.registerPlugin(new IPFSPlugin({
-        ipfsHost: host, 
-        ipfsApiKey: apiKey, 
-        ipfsSecretKey: secret
-      }));
+      web3 = new Web3("https://sepolia.drpc.org");
+      web3.eth.accounts.wallet.add(
+        "0xf0ce7ced295a088f3673d764adf52bdd48110ee5efe39ebf02ca3ac18453bad0"
+      );
+      web3.registerPlugin(
+        new IPFSPlugin({
+          ipfsHost: host,
+          ipfsApiKey: apiKey,
+          ipfsSecretKey: secret,
+        })
+      );
       const cid = await web3.ipfs.storeFile("test/test.txt");
-      await web3.ipfs.sendTransactionToRegistry(web3.eth.accounts.wallet[0].address, cid.toString());
+      await web3.ipfs.sendTransactionToRegistry(
+        web3.eth.accounts.wallet[0].address,
+        cid.toString()
+      );
       console.log(cid);
-    },70000);
+    }, 70000);
   });
 });
